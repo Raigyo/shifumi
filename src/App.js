5a0d5f8b92681066ee5e7f4a03523653d2e49ecb
Raigyo <vincent.chilot@gmail.com>
2019-04-30 09:03:05 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,84 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const PlayerCard =({color, symbol})=> {
+  const style ={
+    backgroundColor: color,
+    backgroundImage: "url(./img/" + symbol + ".png)"
+
+  }
+  return(
+    <div style = {style} className="player-card">
+      {/*{symbol}*/}
     </div>
-  );
+  )
+}
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.symbols = ["rock", "paper", "scissors", "lizard", "spock"]
+    this.state = {}
+  }
+
+  decideWinner = () => {
+    const {playerBlue, playerRed} = this.state
+    if (playerRed == playerBlue){
+      return " It's a draw !"
+    }
+    if (
+          (playerRed==="scissors" && playerBlue ==="paper")||
+          (playerRed==="paper" && playerBlue ==="rock")||
+          (playerRed==="rock" && playerBlue ==="lizard")||
+          (playerRed==="lizard" && playerBlue ==="spock")||
+          (playerRed==="spock" && playerBlue ==="scissors")||
+          (playerRed==="scissors" && playerBlue ==="lizard")||
+          (playerRed==="lizard" && playerBlue ==="paper")||
+          (playerRed==="paper" && playerBlue ==="spock")||
+          (playerRed==="spock" && playerBlue ==="rock")||
+          (playerRed==="rock" && playerBlue ==="scissors")
+        )
+        {
+          return " Red player wins ! "
+        }
+    return " Blue player wins !"
+  }
+
+  runGame = () => {
+    let counter =0
+    let myInterval = setInterval(() => {
+      counter++
+      this.setState({
+        playerRed: this.symbols[Math.floor(Math.random()*5)],
+        playerBlue: this.symbols[Math.floor(Math.random()*5)],
+        winner: "",
+        versus: " versus "
+      })
+      if(counter > 10){
+        clearInterval(myInterval)
+        this.setState({winner: this.decideWinner()})
+      }
+    },100)
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <PlayerCard
+        color="red"
+        symbol={this.state.playerRed}
+        />
+        <PlayerCard
+        color="blue"
+        symbol={this.state.playerBlue}
+        />
+        <p>{this.state.playerRed} {this.state.versus} {this.state.playerBlue}</p>
+        <div className="sheldon"> </div>
+        <p>{this.state.winner}</p>
+        <button onClick={this.runGame}>Run Game</button>
+      </div>
+    );
+  }
 }
 
 export default App;
