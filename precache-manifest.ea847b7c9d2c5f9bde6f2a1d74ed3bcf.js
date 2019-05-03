214b8e6725c0ea51a626cfdb56e6a77867e11daf
Raigyo <vincent.chilot@gmail.com>
2019-05-03 16:41:36 +0200

    Updates

---
+++ precache-manifest.ea847b7c9d2c5f9bde6f2a1d74ed3bcf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae2a0a13f286d19f750b6306620701ff",
+    "url": "/shifumi/index.html"
+  },
+  {
+    "revision": "86e7e2c8f7fccabbf353",
+    "url": "/shifumi/static/css/main.a80aae83.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/shifumi/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "86e7e2c8f7fccabbf353",
+    "url": "/shifumi/static/js/main.688cf520.chunk.js"
+  },
+  {
+    "revision": "b8c43106ecd82e724f0e",
+    "url": "/shifumi/static/js/runtime~main.f9261637.js"
+  }
+]);
